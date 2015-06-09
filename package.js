10e6f76e13fed673538203f2b0d0c0cfa4c41e89
gviscarret <guillaume.viscarret@altic.org>
2015-06-09 11:53:30 +0200

    Initial commit

--- package.js
+++ package.js
@@ -1,14 +1,17 @@
 Package.describe({
   summary: "Login service for France Connect accounts",
-  version: "0.1.0"
+  version: "0.1.0",
+  name: "gvisca75:accounts-franceconnect",
+  git:"https://github.com/gvisca/accounts-franceconnect.git"
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('franceconnect', ['client', 'server']);
+  api.use('gvisca75:franceconnect@0.1.0', ['client', 'server']);
 
   api.addFiles('franceconnect_login_button.css', 'client');
 
